c6034dc8504e970029a6c6f288f28e14c92761c3
fallen90 <slytherin9090@gmail.com>
2016-03-29 10:00:09 +0800

    init

--- app.js
+++ app.js
@@ -1,43 +1,82 @@
-const http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var busboy = require('connect-busboy');
+var chromakey = require('./chromakey');
+var path = require('path');
+var low = require('lowdb')
+var storage = require('lowdb/file-sync')
+var db = low('db.json', { storage });
+var gdrive = require('./gdrive');
+var http = require('http');
+var env = process.env;
+var upload_dir = __dirname + '/public/uploads/';
+var output_dir = __dirname + '/public/outputs/';
 
-let server = http.createServer(function (req, res) {
-  let url = req.url;
-  if (url == '/') {
-    url += 'index.html';
-  }
+app.set('port', (process.env.PORT || 5000));
 
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
+app.set('view engine', 'jade');
+app.set('views', 'public/views')
+app.use(busboy());
 
-  if (url == '/health') {
+// app.use(express.static(__dirname + '/public'));
+app.use('/lib', express.static(__dirname + '/public/lib'));
+
+app.get('/health', function(req, res) {
     res.writeHead(200);
     res.end();
-  } else if (url.indexOf('/info/') == 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store');
-    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-  } else {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end();
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
-      }
-    });
-  }
 });
 
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-  console.log(`Application worker ${process.pid} started...`);
+app.get('/', function(req, res) {
+    res.render('index', { title: 'Hey' });
 });
+
+app.get('/download', function(req, res) {
+    res.render('download', { title: 'Hey' });
+});
+
+app.post('/file-upload', function(req, res) {
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function(fieldname, file, filename) {
+
+        var key = db('images').size() + 1;
+        filename = upload_dir + key + path.extname(filename);
+
+        fstream = fs.createWriteStream(filename);
+        file.pipe(fstream);
+
+        fstream.on('close', function() {
+            // res.redirect('back'); //redirect back to main page
+            // res.send('file uploaded !');
+            chromakey.create(filename, key, function(file) {
+                console.log('file processed', file);
+                db('images').push({
+                    id: key,
+                    file: file
+                });
+                res.end();
+                uploadFile(key);
+            }, function(err) {
+                console.log('failed');
+            });
+        });
+    });
+});
+
+app.set('port', env.NODE_PORT || 3000);
+app.set('ip', env.NODE_IP || 'localhost');
+
+
+http.createServer(app).listen(app.get('port'), app.get('ip'), function() {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'), app.get('port'));
+});
+
+var uploadFile = function(key) {
+    console.log('Uploading...');
+    gdrive.uploadFile(key + '.out.png', key + '.out.png', '0B3zHm_k_51jLWlBUZzVmclJDemc', function(result) {
+        console.log('success!!!', result);
+    }, function(err) {
+        console.log('Error!!!', err);
+    });
+}
---
+++ main.js
@@ -0,0 +1,32 @@
+// var chromakey = require('./chromakey');
+// chromakey.create(process.argv[2], process.argv[3], function(file){
+  
+//   console.log( 'FILE', file );
+
+  
+// }, function(err){
+//     console.log('failed');
+// });
+
+// console.log(new Date().getTime());
+
+// function puts(error, stdout, stderr) { 
+//     if( error )  console.log( error );
+//     //sys.puts(stdout) 
+// }
+
+/*
+image magic commands
+
+composite -geometry 300x250+370+510 butas.png bg.png intermediate.png
+composite fg.png intermediate.png output.png
+
+    fs.unlinkSync(tmp + key + '.masked.png');
+Google Drive API KEY =   AIzaSyDrfuBAC8tiviDDU4uWaSTZNQ4c1iq8qL0
+*/
+
+// var path = require('path');
+// var low = require('lowdb')
+// var storage = require('lowdb/file-sync')
+// var db = low('db.json', { storage })
+// console.log(db('folders').find({name :'day'}));
